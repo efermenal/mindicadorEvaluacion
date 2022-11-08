b4549944e0d6e74e32babace08d71850cc326cd8
Endherson <ferendher@gmail.com>
2022-11-08 00:07:56 -0300

    refactor(network-module): provide to dependency graph Moshi and OkHttp instances

--- app/src/main/java/com/example/mindicadorevaluation/di/NetworkModule.kt
+++ app/src/main/java/com/example/mindicadorevaluation/di/NetworkModule.kt
@@ -2,7 +2,6 @@ package com.example.mindicadorevaluation.di
 
 import com.example.mindicadorevaluation.BuildConfig
 import com.example.mindicadorevaluation.api.LatestIndicatorsJsonAdapter
-import com.example.mindicadorevaluation.api.LatestIndicatorsResponse
 import com.example.mindicadorevaluation.api.MindicadorApi
 import com.example.mindicadorevaluation.core.utils.Constants.BASE_URL
 import com.squareup.moshi.Moshi
@@ -21,20 +20,16 @@ class NetworkModule {
     @Provides
     @Singleton
     @Named("api")
-    fun provideRetrofit(): Retrofit {
-
-        val moshi = Moshi.Builder()
-            .add(LatestIndicatorsJsonAdapter())
-            .build()
-
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(createClient())
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
-    private fun createClient(): OkHttpClient {
+    @Provides
+    fun providesOkHttpClient(): OkHttpClient {
         val okHttpClientBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
         if (BuildConfig.DEBUG) {
             val loggingInterceptor =
@@ -45,7 +40,15 @@ class NetworkModule {
     }
 
     @Provides
-    fun trackerListService(@Named("api") retrofit: Retrofit): MindicadorApi =
+    fun providesMiIndicadorApi(@Named("api") retrofit: Retrofit): MindicadorApi =
         retrofit.create(MindicadorApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(LatestIndicatorsJsonAdapter())
+            .build()
+    }
 
 }
